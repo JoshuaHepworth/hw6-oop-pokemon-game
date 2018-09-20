85974dd45957cdaa7f7ff0b48b07a996d32215ce
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-20 10:50:17 -0500

    player and computer recive three random cards

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 console.log('pokemon');
 
 // ----------------PLAYER OBJECT
-const cardsInPlay = [];
-const player = {
-		name: "Josh",
-		cards: 'cards',
-		recieveCard: function(){
-		// cardsInPlay = Math.floor(Math.random()*100)
-		console.log(`${player.name} has recieved ${cardsInPlay}`)
-		}
-		playCard: function(){
-		console.log(`${player.name} has chosen`)
-		}
-		checkStats: function(){
-			console.log(`${player.name} has a score of`)
-		}
-}
-	console.log(player.recieveCard())
-	console.log(player);
+// const cardsInPlay = [];
+// const player = {
+// 		name: "Josh",
+// 		recieveCard: function(){
+// 		// cardsInPlay = Math.floor(Math.random()*100)
+// 		console.log(`${player.name} has recieved ${cardsInPlay}`)
+// // 		}
+// // 		playCard: function(){
+// // 			console.log(`${player.name} has chosen`)
+// // 			return
+// // 		}
+// // 		checkStats: function(){
+// // 			console.log(`${player.name} has a score of`)
+// // 			return
+// // 		}
+// // }
+// 	console.log(player.recieveCard())
+// 	console.log(player);
 	
 // console.log(player)
 // player.recieveCard();
@@ -38,21 +39,19 @@ const player = {
 
 // see the cards that they have played in the past.
 //-------------------THE COMPUTER
-const computer = {
-	name: 'computer',
-	cards: 'cards',
-	stats: 'stats',
-	recieveCard: function(){
+// const computer = {
+// 	name: 'computer',
+// 	recieveCard: function(){
 	
-	// cardsInPlay = Math.floor(Math.random()*100)
-		console.log(`${computer.name} has recieved ${cardsInPlay}!`)
-		}
-		playCard: function(){
-			console.log(`${computer.name} has played ${cardsInPlay}!`)
-		}
+// 	// cardsInPlay = Math.floor(Math.random()*100)
+// 		console.log(`${computer.name} has recieved ${cardsInPlay}!`)
+// 		}
+// 		playCard: function(){
+// 			console.log(`${computer.name} has played ${cardsInPlay}!`)
+// 		}
 
 
-}
+// }
 //-------------------THE UI
 
 // The "UI"
@@ -93,13 +92,17 @@ const computer = {
 
 // stop once there are no cards left or not enough to deal 3 to each the player and computer
 
-const game = {
+// const game = {
+// 	cardsInPlay = []
+// 	pullCards(){
+// 		for(let i = 0; i < cards.length; i++){
+// 			this.cardsInPlay.push(cards[Math.floor(Math.random() * cards.length)])
+// 		}
+// 	}
+// }
+// game.pullCards();
 
-
-}
-
-
-let cards =
+const cards =
 [
   {
     name: "Bulbasaur",
@@ -156,27 +159,89 @@ let cards =
     name: "Weedle", 
     damage: 40
   }
-]
-for(let key in cards) {
-	console.log(cards[key])
+];
+// ----------------OBJECTS player/computer
+
+const player = {
+	name: 'Josh',
+	points: 0,
+	cardsDrawn: [],
+	cardsInPlay: []
 }
+const computer = {
+	name: 'Computer',
+	points: 0,
+	cardsDrawn: [],
+	cardsInPlay: []
+}
+// //--------------DECK!!!!!
 
-console.log(cards);
+const deck = [];
 
+for (let i = 0; i < cards.length; i++) {
+	deck.push(cards[i]);
+}
+console.log(deck);
+
+
+//--------------DRAW CARDS FUNCTION
+//will go in cardsDrawn array above
+const drawCards = () => {
+// //loop through deck, grab random 3 cards. push them to cardsDrawn array. loop through the deck for both the computer and the player so they each get a random 3 from the deck
+	for( let i = 0; i < 3; i++) {
+		let randCard = Math.floor(Math.random() * deck.length);
+			player.cardsDrawn.push(deck[randCard]);
+			deck.splice(randCard, 1);
+
+		}//player is recieving random cards of 3 now	
+		console.log(`${player.name} recieved ${player.cardsDrawn[0].name} and ${player.cardsDrawn[1].name} and ${player.cardsDrawn[2].name}`)
+}
+		for( let i = 0; i < 3; i++) {
+			let randCard = Math.floor(Math.random() * deck.length);
+			computer.cardsDrawn.push(deck[randCard]);
+			deck.splice(randCard, 1);
+		}	
+			console.log(`${computer.name} recieved ${computer.cardsDrawn[0].name} and ${computer.cardsDrawn[1].name} and ${computer.cardsDrawn[2].name}`)
+
+		
+
+drawCards();
+
+
+//---------------PLAY CARDS
+// let playedCards = () => {
+
+// }
+///----------------fight
+// if(player1.)
+
+
+
+//three random cards pulled from cards object.
+// for(let i = 0; i < 3; i++){
+// shuffleDeck = cards[Math.floor(Math.random() * cards.length)]
+// 	// console.log(cardsInPlay)
+// 	console.log(shuffleDeck)
+// }
+
+// console.log(shuffleDeck)
+
+
+// shuffleDeck pop ----> player deck && cardsInPlay
 
 //----------------SHUFFLE DECK
 
-const shuffle = (array) => {
-	for (let i = array.length -1; )
-}
+// const shuffle = (array) => {
+// 	for (let i = array.length -1; )
+// }
 
 
 
+//compare cards function
 
+//deal cards function in game object
 
-
-
-
+//
 
 
 
